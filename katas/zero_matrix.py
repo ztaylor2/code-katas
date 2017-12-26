ee300aec09ea90aada356ca0c6555f1290a6795a
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-25 23:11:35 -0800

    Built files and func defs for zero matrix.

---
+++ katas/zero_matrix.py
@@ -0,0 +1,12 @@
+"""Zero matrix: Write an algorithm such that if an element in an MxN
+matrix is 0, its entire row and column are set to 0.
+
+CTCI 1.8.
+"""
+
+
+def zero_matrix(a_matrix):
+    """Zero matrix."""
+    # store all zeros in dict
+
+    # covert respective rows/cols to zeros
