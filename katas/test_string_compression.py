523e3f4518c5e0aaedad65b58ddcea42cae505c4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-03 15:48:26 -0500

    Passing test for when compression doesnt shorten, passing long compression test.

--- katas/test_string_compression.py
+++ katas/test_string_compression.py
@@ -11,3 +11,15 @@ def test_compression_multiple_chars():
     """."""
     from string_compression import string_compression
     assert string_compression('aaabbb') == 'a3b3'
+
+
+def test_shorter_without_compression():
+    """Test that the original string is returned if the compression doesn't make it shorter."""
+    from string_compression import string_compression
+    assert string_compression('abcd') == 'abcd'
+
+
+def test_verbose_compression():
+    """Test many charactor compression."""
+    from string_compression import string_compression
+    assert string_compression('aaaabbbcdddeefffff') == 'a4b3c1d3e2f5'
