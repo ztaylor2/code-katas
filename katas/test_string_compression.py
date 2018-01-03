974e61ed2b3dc786710a6fa3323a3ac5489dc458
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-03 10:30:45 -0500

    wrote multiple char test for string compression.

--- katas/test_string_compression.py
+++ katas/test_string_compression.py
@@ -5,3 +5,9 @@ def test_compression_simple():
     """."""
     from string_compression import string_compression
     assert string_compression('aaaa') == 'a4'
+
+
+def test_compression_multiple_chars():
+    """."""
+    from string_compression import string_compression
+    assert string_compression('aaabbb') == 'a3b3'
