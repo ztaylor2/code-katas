5235ad28915bbcbff022aeccef7957fff8ed6068
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-03 10:28:48 -0500

    string compression passing test.

--- katas/string_compression.py
+++ katas/string_compression.py
@@ -12,16 +12,19 @@ if compressed string is not smaller than original string return original string.
 def string_compression(uncompressed_string):
     """Simple string compression of repeated chars."""
     compressed_string = ''
-    for i, char in enumerate(uncompressed_string):
-        compressed_string += char
-        while True:
-            x = 0
-            if (char == uncompressed_string[i + x]):
+    i = 0
+    x = 0
+    while i + x < len(uncompressed_string):
+        # import pdb; pdb.set_trace()
+        compressed_string += uncompressed_string[i]
+        x = 0
+        while i + x < len(uncompressed_string):
+            if (uncompressed_string[i] == uncompressed_string[i + x]):
                 x += 1
             else:
                 break
-        uncompressed_string += str(x)
+        compressed_string += str(x)
         i += x
-    # return original string if length not improved
     if len(uncompressed_string) <= len(compressed_string):
         return uncompressed_string
+    return compressed_string
