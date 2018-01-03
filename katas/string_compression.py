64bc6db227b5a859f6efac64b720574b4c9f7176
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-03 16:13:58 -0500

    added alternate solution.

--- katas/string_compression.py
+++ katas/string_compression.py
@@ -1,11 +1,11 @@
-"""Implement a method to perfom a string compressoin using counts of repeated charactors.
+"""Implement a method to perfom a string compression using counts of repeated charactors.
 
 CTCI 1.6
 
 ex: input: aabcccccaaa
     output: a2b1c5a3
 
-if compressed string is not smaller than original string return original string.
+if compressed string not smaller than original string return original string.
 """
 
 
@@ -27,3 +27,37 @@ def string_compression(uncompressed_string):
     if len(uncompressed_string) <= len(compressed_string):
         return uncompressed_string
     return compressed_string
+
+"""
+Python solution from http://rosettacode.org/wiki/Run-length_encoding#Python:
+
+Note: this output is not exactly what CTCI is looking for.
+
+def encode(input_string):
+    count = 1
+    prev = ''
+    lst = []
+    for character in input_string:
+        if character != prev:
+            if prev:
+                entry = (prev,count)
+                lst.append(entry)
+                #print lst
+            count = 1
+            prev = character
+        else:
+            count += 1
+    else:
+        entry = (character,count)
+        lst.append(entry)
+    return lst
+"""
+
+def string_comp_refactored(uncompressed_string):
+    """Refactor of string compression method."""
+    count = 1
+    prev = ''
+    compressed_string = ''
+    for char in uncompressed_string:
+        if char != prev:
+            
