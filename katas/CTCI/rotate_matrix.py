5999c7634be6b2f291ee3133de887c03939a4bf7
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-04 15:01:20 -0500

    wrote test and example for rotate-matrix.

--- katas/CTCI/rotate_matrix.py
+++ katas/CTCI/rotate_matrix.py
@@ -5,6 +5,20 @@ Rotate Matrix
 
 Given an image represented by an NxN matrix where each pizel in the
 image is 4 bytes, write a method to rotate the image by 90 degrees.
+
+ex:
+
+input: [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
+output: [
+            [7, 4, 1],
+            [8, 5, 2],
+            [9, 6, 3]
+        ]
 """
 
 
