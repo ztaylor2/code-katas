fb5cdb84055d5ada78c224af796d547db50180de
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-04 14:31:12 -0500

    built scafolding for rotate matrix.

---
+++ katas/CTCI/rotate_matrix.py
@@ -0,0 +1,13 @@
+"""
+CTCI 1.7
+
+Rotate Matrix
+
+Given an image represented by an NxN matrix where each pizel in the
+image is 4 bytes, write a method to rotate the image by 90 degrees.
+"""
+
+
+def rotate_matrix(image_string):
+    """Rotate an image by 90 degrees."""
+    pass
---
+++ katas/CTCI/test_rotate_matrix.py
@@ -0,0 +1,1 @@
+"""Test rotate matrix."""
