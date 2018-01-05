c1b2dcdbf284e6ad0d1dd7e0c2bc73ab52f6f834
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-05 12:47:16 -0800

    palendrome is passing for even true and false lists and long list.

--- katas/CTCI/chapter_2/test_palendrome.py
+++ katas/CTCI/chapter_2/test_palendrome.py
@@ -21,3 +21,42 @@ def test_palendrome_returns_true():
     test_ll.push('b')
     test_ll.push('a')
     assert linked_palendrome(test_ll) is True
+
+
+def test_palendrome_even_list_true():
+    """Test with even list and true."""
+    from kth_to_last import LinkedList
+    from palendrome import linked_palendrome
+    test_ll = LinkedList()
+    test_ll.push('a')
+    test_ll.push('b')
+    test_ll.push('b')
+    test_ll.push('a')
+    assert linked_palendrome(test_ll) is True
+
+
+def test_palendrome_even_list_false():
+    """Test with even list and false."""
+    from kth_to_last import LinkedList
+    from palendrome import linked_palendrome
+    test_ll = LinkedList()
+    test_ll.push('a')
+    test_ll.push('b')
+    test_ll.push('c')
+    test_ll.push('a')
+    assert linked_palendrome(test_ll) is False
+
+
+def test_palendrome_long_list_true():
+    """Test with long list and true."""
+    from kth_to_last import LinkedList
+    from palendrome import linked_palendrome
+    test_ll = LinkedList()
+    test_ll.push('a')
+    test_ll.push('b')
+    test_ll.push('c')
+    test_ll.push('d')
+    test_ll.push('c')
+    test_ll.push('b')
+    test_ll.push('a')
+    assert linked_palendrome(test_ll) is True
