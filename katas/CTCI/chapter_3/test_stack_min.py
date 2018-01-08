03601eae8c5a2b0f73b66795df71d9c0cd5034fb
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-08 13:46:47 -0800

    min stack passing test for returning correct min after popping previous min.

--- katas/CTCI/chapter_3/test_stack_min.py
+++ katas/CTCI/chapter_3/test_stack_min.py
@@ -28,3 +28,14 @@ def test_min_method(stack):
     stack.push(3)
     stack.push(4)
     assert stack.min() == 1
+
+
+def test_min_after_popping_min(stack):
+    """Test return min value after popping min val."""
+    stack.push(5)
+    stack.push(7)
+    stack.push(43)
+    stack.push(4)
+    assert stack.min() == 4
+    assert stack.pop() == 4
+    assert stack.min() == 5
