149f47c22abc24adae7a001614a5b503ec0fdd0e
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-09 18:58:30 -0800

    linked list passing test for init with list.

--- katas/CTCI/chapter_4/test_linked_list.py
+++ katas/CTCI/chapter_4/test_linked_list.py
@@ -19,3 +19,12 @@ def test_add_method(ll):
     assert ll.head.val == 3
     assert ll.head.next.val == 2
     assert ll.head.next.next.val == 1
+
+
+def test_init_with_list():
+    """Test initing linked list with a list."""
+    from linked_list import LinkedList
+    ll = LinkedList([1, 2, 3])
+    assert ll.head.val == 3
+    assert ll.head.next.val == 2
+    assert ll.head.next.next.val == 1
