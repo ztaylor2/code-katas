8672bf31f1a8be49972f2e712a37a7cc58f4e5b8
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-09 19:30:52 -0800

    list of depths not working.

--- katas/CTCI/chapter_4/test_list_of_depths.py
+++ katas/CTCI/chapter_4/test_list_of_depths.py
@@ -11,7 +11,16 @@ def test_list_of_depths():
     bst.insert(7)
     bst.insert(2)
     bst.insert(4)
-    bst.insert(5)
+    bst.insert(6)
     bst.insert(8)
     list_of_lls = list_of_depths(bst)
     assert list_of_lls[0].head.val == 5
+    assert not list_of_lls[0].head.next
+    assert list_of_lls[1].head.val == 2
+    assert list_of_lls[1].head.next.val == 4
+    assert list_of_lls[1].head.next.next.val == 6
+    assert list_of_lls[1].head.next.next.next.val == 8
+    assert list_of_lls[1].head.next.next.next.next.val == 3
+    assert list_of_lls[1].head.next.next.next.next.next.val == 7
+    assert list_of_lls[2].head.val == 2
+    assert list_of_lls[2].head.next.val == 4
