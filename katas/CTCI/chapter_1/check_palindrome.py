70a1654ef33f3183a056bdb0a3a80520e0f496af
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-11 16:08:48 -0800

    check palindrome method is passing test.

--- katas/CTCI/chapter_1/check_palindrome.py
+++ katas/CTCI/chapter_1/check_palindrome.py
@@ -3,6 +3,7 @@
 
 def is_palindrome(input_string):
     """Check if a string is a palindrome."""
+    input_string = input_string.lower()
     for i in range(len(input_string) // 2):
         if input_string[i] != input_string[-(i + 1)]:
             return False
