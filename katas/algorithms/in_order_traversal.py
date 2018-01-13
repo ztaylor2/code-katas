b2a84fc3b1885e45ad2961aab3a34f7a684fd8d2
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-12 16:39:26 -0800

    built out pre order, post order, and in order traversal methods.

--- katas/algorithms/in_order_traversal.py
+++ katas/algorithms/in_order_traversal.py
@@ -3,25 +3,15 @@
 
 def in_order_traversal(node):
     """An in order traversal."""
-    visited = set()
     return_list = []
 
     def in_order(node):
-        """."""
-        if not node.right and not node.left:
-            return_list.append(node.val)
-            visited.add(node)
-            in_order(node.parent)
-        if node.left and node.left not in visited:
+        """Recursive."""
+        if node.left:
             in_order(node.left)
-        if node.right and node.right not in visited:
-            visited.add(node)
-            return_list.append(node.val)
+        return_list.append(node.val)
+        if node.right:
             in_order(node.right)
-
-        if node.right in visited and node.left in visited:
-            if node.parent:
-                in_order(node.parent)
 
     in_order(node)
     return return_list
