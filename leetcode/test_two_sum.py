0c433835172a65e50fb5a9bb977fe0a756be7e21
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-13 16:06:22 -0800

    two_sum passing another test.

--- leetcode/test_two_sum.py
+++ leetcode/test_two_sum.py
@@ -5,3 +5,9 @@ def test_two_sum():
     """."""
     from two_sum import two_sum
     assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+
+
+def test_two_sum_more():
+    """."""
+    from two_sum import two_sum
+    assert two_sum([1, 2, 11, 4, 5, 6, 7, 8], 8) == [0, 6]
