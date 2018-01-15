b9df2f5c487c56b9a0b0ef5a431d94693f4d4faa
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-15 13:51:00 -0800

    typed out boggle solution.

--- katas/algorithms/test_depth_first_search.py
+++ katas/algorithms/test_depth_first_search.py
@@ -1,9 +1,9 @@
 """Test depth first search."""
 
 
-def test_depth_first_search():
-    """Test depth first search."""
+def test_dfs():
+    """."""
     from bst import BinarySearchTree
-    from depth_first_search import depth_first_search
+    from depth_first_search import dfs_wrapper
     bst = BinarySearchTree([5, 3, 7, 2, 4, 6, 8])
-    assert depth_first_search(bst.root) == [5, 3, 2, 4, 7, 6, 8]
+    assert dfs_wrapper(bst.root) == [5, 3, 2, 4, 7, 6, 8]
