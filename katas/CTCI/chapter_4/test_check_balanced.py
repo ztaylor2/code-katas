36d62ca053161c6d072c39a48f45270013914301
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-22 13:54:01 -0800

    balanced tree passing test for true.

--- katas/CTCI/chapter_4/test_check_balanced.py
+++ katas/CTCI/chapter_4/test_check_balanced.py
@@ -7,3 +7,11 @@ def test_balanced():
     from bst import BinarySearchTree
     bst = BinarySearchTree([1, 2, 3, 4, 5])
     assert is_balanced(bst.root) is False
+
+
+def test_balanced_true():
+    """."""
+    from check_balanced import is_balanced
+    from bst import BinarySearchTree
+    bst = BinarySearchTree([5, 3, 7, 2, 4, 6, 8])
+    assert is_balanced(bst.root) is True
